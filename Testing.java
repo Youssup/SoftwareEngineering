1bf49d5abf549fd9aa4f7c78a4de36930516d628
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-01-23 21:03:39 -0500

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,6 +1,8 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.printf("\n", "hello");
-        System.out.printf("\n",1);
+        /*This is wrong as it fails the whitespace checkstyle
+        as commas need spaces after
+        System.out.printf("\n",1);*/
     }
 }
