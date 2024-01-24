3458046f5a7628a3dd11e0cb09b5a258efb7964c
Brandon Stewart <113845525+sbrandon96@users.noreply.github.com>
2024-01-23 20:32:04 -0500

    Added checkstyleTest file
    Added file to check our new rules

---
+++ checkstyleTest.java
@@ -0,0 +1,10 @@
+class checkstyleTest{
+    public static void main(String [] args){
+        System.out.println("Java is so cool!");
+
+        System.out.println("I imported Scanner, but I didn't use it. Oops");
+
+        System.out.println("Why did I import swing? I don't use it!");
+
+    }
+}
