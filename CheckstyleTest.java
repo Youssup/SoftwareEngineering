ca68bbb4e01de7746a050b08b3fd97fe4fa801c8
Brandon Stewart <113845525+sbrandon96@users.noreply.github.com>
2024-01-23 20:47:58 -0500

    Added file to test rule
    Added a java file to test UnusedImports rule

---
+++ CheckstyleTest.java
@@ -0,0 +1,14 @@
+//Testing Brandon's UnusedImports rule below
+import java.util.Scanner;
+import javax.swing.*;
+
+class CheckstyleTest{
+    public static void main(String [] args){
+        System.out.println("Java is so cool!");
+
+        System.out.println("I imported Scanner, but I didn't use it. Oops");
+
+        System.out.println("Why did I import swing? I don't use it!");
+
+    }
+}
