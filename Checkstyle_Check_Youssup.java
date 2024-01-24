6ee9dbb0a2e1b87381bfbd9ea210edc830242348
Yoosup Song <130710364+Youssup@users.noreply.github.com>
2024-01-23 20:57:12 -0500

    Update and renamed the file
    Fixed the error related to the line being longer than 100

--- Checkstyle_Check_Youssup.java
+++
@@ -1,5 +0,0 @@
-public class Checkstyle_Check_Youssup {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!");
-    }
-}
