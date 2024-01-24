1205301fd8ce5e822e65160ede445015f636e14d
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-01-23 20:49:46 -0500

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,6 +1,8 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.printf("\n", "hello");
-        System.out.printf("\n",1);
+       
+        /* This is an Error as the comma doesn't have a space in between
+            System.out.printf("\n",1);*/
     }
 }
