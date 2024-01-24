7abfd100c93408a7d13bd6fbee447fbb85fe5fc3
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-01-23 20:59:54 -0500

    Delete Testing.java (#15)

--- Testing.java
+++
@@ -1,8 +0,0 @@
-public class Testing {
-    public static void main(String[] args) {
-        System.out.printf("\n", "hello");
-       
-        /* This is an Error as the comma doesn't have a space in between
-            System.out.printf("\n",1);*/
-    }
-}
