fac57800eaea4e9944d201212bac22b5f6e463a7
Yousup <youssupsong@gmail.com>
2024-02-02 21:32:16 -0500

    Created Compute Engine
    Mediates between the User and Data Storage and computed the User's data

---
+++ API_Folder/ComputeEngine.java
@@ -0,0 +1,33 @@
+public class ComputeEngine{
+    //Sending the Computed data to user
+    public Data sendToUser(Data inputData)
+    {
+        //send inputData to data storage
+        Data translatedData = sendToDataStorage(inputData);
+        //compute translated data that was sent from data storage
+        int computedData = compute(translatedData);
+        //send that computed data to the client/user
+        Data outputData = DataStorage.userTranslate(computedData);
+
+    }
+
+    //sending the data retrived from user, to the Data Storage
+    public Data sendToDataStorage(Data inputData)
+    {
+        /*send user data to data storage and retrieve the 
+        translated data that the Compute Engine can understand*/
+        Data translatedData = DataStorage.userTranslate(inputData);
+        /*return the translated data*/
+        return translatedData;
+    }
+
+    //Computing the data sent from the user
+    public Data compute(Data inputData)
+    {
+        //Ackerman function implementation
+    }
+}
+
+public interface ComputeEngineAPI{
+
+}
