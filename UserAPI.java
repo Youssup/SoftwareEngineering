3c39cfe532d0c8742f7f896e48fe54bb7fe0319b
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-02 20:12:34 -0500

    Add files via upload

---
+++ UserAPI.java
@@ -0,0 +1,19 @@
+public class UserAPI implements ComputerEngineAPI {
+    /*recive request from user method
+
+    Sever to client and client to Server */
+
+    public User JobInfoSend(User inputData){
+        //give info on job and retrives the data from the Computer Engine
+        User outputData= ComputerEngine.sending(InputData);
+        
+        //it will return the data from the Computer Engine
+        return outputData;
+    }
+
+}
+
+public interface UserInterface {
+   User JobinfoSend(User inputData);
+   
+}
