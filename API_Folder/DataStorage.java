27dcd400f4dad133e0e231d23187d750007a2186
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-07 18:02:00 -0500

    Revert "Delete API_Folder directory (#27)"
    This reverts commit c8ddcf16ad0a2984c0f39d6301b138063c9c67c2.

---
+++ API_Folder/DataStorage.java
@@ -0,0 +1,25 @@
+public class DataStorage implements DataStorageInterface{
+    //Receive computed data from CE and translate
+    public Data userTranslate(Data data){
+        //Some magical algorithm to translate data will exist below
+        //This is the 'write' poriton of the DS
+        Data translatedData = computeDataTranslate(data);
+        return translatedData;
+    }
+
+    //Send data back to CE translated
+    public Data computeDataTranslate(Data translatedData){
+        //Some magical algorithm will place the translated data
+        //into the correct format for the CE to read
+        Data data = translatedData;
+        return data;
+    }
+
+}
+
+public interface DataStorageInterface {
+    //Method to translate data for user
+    DataStorage userTranslate(Data data);
+    //Method to send data to CE
+    DataStorage computeDataTranslate(Data data);
+}
