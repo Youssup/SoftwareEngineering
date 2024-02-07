73148f0d3aaffb226e0f7ab09e85ff27522a83a8
Yousup <youssupsong@gmail.com>
2024-02-07 18:03:04 -0500

    Update ComputeEngine.java

--- API_Folder/ComputeEngine.java
+++ API_Folder/ComputeEngine.java
@@ -1,7 +1,8 @@
 public class ComputeEngine implements ComputeEngineAPI {
     //Computing the data sent from the user
-    public Data compute(Data inputData) {
+    public int compute(int[] inputData) {
         computeDataTranslate(Client.readData(), Client.readDelimiter());
         //Ackerman function implementation
+        return result;
     }
 }
