a76c7ff989713ec839a3f880b3de22a6137972ab
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-07 17:16:56 -0500

    Add files via upload

---
+++ API_Folder/DataStorageAPI.java
@@ -0,0 +1,17 @@
+import javax.xml.crypto.Data;
+
+public interface DataStorageAPI {
+    //Receive computed data from ComputeEngine
+    //and translate so that the user can understand
+    public int[] userTranslate(Data translatedData, char delimiter);
+    //retrieve computed data
+
+
+    //Translate the data so that the Computer Engine can understand
+    public int[] computeDataTranslate(Data inputData, char delimiter);
+    //An algorithm to seperate the data using the delimiter
+
+}
+//translated data = data that the Compute Engine can understand
+//delimiter = a character that separates the data
+//input data = data from the user
