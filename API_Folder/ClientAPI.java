5fd5da7012cc597cd6cd6fb04ad3b472c0540474
Yoosup Song <130710364+Youssup@users.noreply.github.com>
2024-02-07 18:05:16 -0500

    Overhaul feature branch (#30)
    * Redoing the SWE API/Project
    
    * Update Client.java to be able to implement the ClientAPI
    
    * Update ComputeEngine.java with implementing the API
    
    * Update DataStorage.java implements API and changing to keep checkstyle
    
    * Add files via upload
    
    * Update Notes
    
    * Update Client.java
    
    * Update ComputeEngine.java
    
    ---------
    
    Co-authored-by: J03lMATH <157427495+J03lMATH@users.noreply.github.com>

---
+++ API_Folder/ClientAPI.java
@@ -0,0 +1,11 @@
+
+import javax.xml.crypto.Data;
+
+public interface ClientAPI {
+//the front end of the program where the user can interact
+    //read the data from the user
+    public Data readData();
+
+     //read the delimiter from the user
+     public char readDelimiter();
+}
