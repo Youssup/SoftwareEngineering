5a6b4e93415766fa75c901917afcd11493b3cfbc
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-09 17:21:00 -0500

    Delete API_Folder directory

--- API_Folder/ComputeEngine.java
+++
@@ -1,8 +0,0 @@
-public class ComputeEngine implements ComputeEngineAPI {
-    //Computing the data sent from the user
-    public int compute(int[] inputData) {
-        computeDataTranslate(Client.readData(), Client.readDelimiter());
-        //Ackerman function implementation
-        return result;
-    }
-}
--- API_Folder/ComputeEngineAPI.java
+++
@@ -1,6 +0,0 @@
-import javax.xml.crypto.Data;
-
-public interface ComputeEngineAPI {
-//Computing the data sent from the user
-    public Data compute(Data inputData);
-}
