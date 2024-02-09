105f2752a485167d9d9346fc3c3ae7d2c56fa21c
Brandon Stewart <113845525+sbrandon96@users.noreply.github.com>
2024-02-09 17:39:58 -0500

    Add DataStoreTests.java
    Added Mockito cross paths, enabled Java Tests, then added the DataStoreTests.java. All tests run good so far. Please update with any necessary changes, thank you.

---
+++ test/DataStoreTests.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+import java.util.ArrayList;
+
+//We will use Mockito to simnuate the behavior of our classes
+
+public class DataStoreTests {
+
+    @Test
+    public void testRead() {
+        //Mock InputConfig
+        InputConfig inputConfig = mock(InputConfig.class);
+
+        //Create a mock DataStore
+        DataStore dataStore = mock(DataStore.class);
+
+        //Set up the case we will test
+        //Read in the inputConfig and return something
+        when(dataStore.read(inputConfig)).thenReturn(new ArrayList<Integer>());
+
+        //Test the read method(I had to return an ArrayList<Integer> because I couldn't return an Iterable<Integer> object)
+        Iterable<Integer> results = dataStore.read(inputConfig);
+
+    }
+
+    @Test
+    public void testAppendSingleResult() {
+        //Create a mock OutputConfig
+        OutputConfig outputConfig = mock(OutputConfig.class);
+        //Basic result to test
+        String result = "Result";
+
+        //Create a mock DataStore object
+        DataStore dataStore = mock(DataStore.class);
+
+        //Set up mock case for the test
+        when(dataStore.appendSingleResult(outputConfig, result)).thenReturn(mock(WriteResult.class));
+
+        //Test the appendSingleResult method
+        WriteResult writeResult = dataStore.appendSingleResult(outputConfig, result);
+
+    }
+}
