08fde08f8d890eced5a9a877227886048d9ca6a0
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-09 18:01:31 -0500

    new files

---
+++ ProjectFolder/ClientAPI.java
@@ -0,0 +1,14 @@
+public class ClientAPI {
+    public int getInputConfig(){
+        int input=5;
+        return input;
+    }
+	public int getOutputConfig(){
+        int output=5;
+        return output;
+    }
+	public char getDelimeter(){
+        char Deli= 'A';
+        return Deli;
+    }
+}
