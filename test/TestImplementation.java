b8502aff1aae70d95c4ae5e523123be65649b7cd
Brandon Stewart <113845525+sbrandon96@users.noreply.github.com>
2024-02-10 20:41:34 -0500

    First Staging of the TestImplementation
    For part 4. In-progress

---
+++ test/TestImplementation.java
@@ -0,0 +1,56 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class TestOutputConfig{
+    public List<String> output;
+
+    public TestOutputConfig(){
+        output = new ArrayList<String>();
+    }
+    public List<String> getOutputData(){
+        return output;
+    }
+    public void write(String data){
+        output.add(data);
+    }
+}
+
+class TestInputConfig{
+    public List<Integer> input;
+
+    public TestInputConfig(List<Integer> data) {
+        this.input = data;
+    }
+}
+
+//This simulates the DataStorageAPI class
+class TestDataStorageAPI{
+    public Iterable<Integer> testRead(TestInputConfig inputConfig){
+        return inputConfig.input;
+    }
+
+    public WriteResult testAppendSingleResult(TestOutputConfig outputConfig, String result){
+        outputConfig.write(result);
+        return new WriteResult();
+    }      
+}
+
+public class TestImplementation{
+    //This main method will depict the infrastructure of the DataStorageAPI implementations
+    public static void main(String[] args){
+
+        List<Integer> data = new ArrayList<Integer>();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        TestInputConfig inputConfig = new TestInputConfig(data);
+
+        TestDataStorageAPI dataStore = new TestDataStorageAPI();
+
+        dataStore.testRead(inputConfig);
+
+        TestOutputConfig outputConfig = new TestOutputConfig();
+        outputConfig.write("Hey dude!");
+
+    }
+}
