a4eceb53ea555b749aa61fb4ca4f813de4634754
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-02-10 20:03:43 -0500

    making computeEngine Yellow

--- src/ComputeEngineAPI.java
+++ src/ComputeEngineAPI.java
@@ -8,14 +8,14 @@ public class ComputeEngineAPI implements ComputeEngine, ComputationCoordinator
     //needs the to decipher what the job is from the client
     private ClientAPI client;
     //needs to retrieve the single outputs from the data storage
-    priavte DataStorageAPI dataStorage;
+    private DataStorageAPI dataStorage;
     //Computes the result using the ackermann function
     public String compute(int value) 
     {
         return "1";
     }
     //Delegating jobs to the Client, ComputeEngine, and the DataStorage
-    ComputeResult compute(ComputeRequest request)
+    public ComputeResult compute(ComputeRequest request)
     {
         return ComputeResult.SUCCESS;
     }
