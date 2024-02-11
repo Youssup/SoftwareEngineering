719b6bc426979b1a6e15884cb15b159d61438822
Yousup <youssupsong@gmail.com>
2024-02-10 19:15:13 -0500

    Updated CE

--- test/TestComputeEngine.java
+++ test/TestComputeEngine.java
@@ -1,4 +1,16 @@
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import org.junit.Test;
+import src.ComputeEngineAPI;
+
 public class TestComputeEngine 
 {
-    
+    @Test
+    public void testComputeEngine() 
+    {
+        ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
+        when(ce.compute(any(Integer.class))).thenReturn("1");
+        ce.compute(1);
+    }
 }
