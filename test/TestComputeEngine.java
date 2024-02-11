c83dcc8b9a97cca970b1333e096e4b6b65b0f3fd
Yousup <youssupsong@gmail.com>
2024-02-10 19:47:41 -0500

    Auto stash before merge of "Development-Branch" and "origin/Development-Branch"

--- test/TestComputeEngine.java
+++ test/TestComputeEngine.java
@@ -1,5 +1,7 @@
+package test;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.any;
 import org.junit.Test;
 import src.ComputeEngineAPI;
@@ -7,10 +9,18 @@ import src.ComputeEngineAPI;
 public class TestComputeEngine 
 {
     @Test
-    public void testComputeEngine() 
+    public void testAlgorithmCompute() 
     {
         ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
         when(ce.compute(any(Integer.class))).thenReturn("1");
-        ce.compute(1);
+        ce.compute("1");
+    }
+    @Test
+    public void testRequest() 
+    {
+        ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
+        when(ce.compute(any(ComputeRequest.class)).thenReturn(ComputeResult.SUCCESS);
+        ComputeRequest request = new ComputeRequest();
+        ce.compute(request);
     }
 }
