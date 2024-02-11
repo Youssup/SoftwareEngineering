c1a25fdd00529727a9736c46137ad75f4d142abf
Yousup <youssupsong@gmail.com>
2024-02-10 20:56:43 -0500

    CE Engine Integration test
    Attempted

--- test/TestComputeEngineAPI.java
+++ test/TestComputeEngineAPI.java
@@ -1,10 +1,19 @@
 package test;
 import org.mockito.Mockito;
+
+import project.OutputConfig;
+
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.any;
 import org.junit.Test;
+import org.junit.platform.reporting.shadow.org.opentest4j.reporting.events.core.Data;
+
 import src.ComputeEngineAPI;
+import src.DataStorageAPI;
 
 public class TestComputeEngineAPI
 {
@@ -24,3 +33,23 @@ public class TestComputeEngineAPI
         ce.compute(request);
     }
 }
+
+class ComputeEngineIntegrationTest
+{
+    DataStorageAPI datastore = new DataStorageAPI();
+    ComputeEngineAPI ce = new ComputeEngineAPI();
+    public void testComputeEngineIntegration() 
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(10);
+        list.add(25);                                                              //should be some input config/input?
+        datastore.appendSingleResult(any(OutputConfig.class), ce.compute(datastore.read(list);));
+    }
+    @Test
+    public void validation(OutputConfig output)
+    {                           //user input
+        if(output == ce.compute(output))
+    }
+}
+//I know this is all wrong I'm lost
