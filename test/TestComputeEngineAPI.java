cbadafab73de9ff121d1251619b1ee76d268736c
Yousup <youssupsong@gmail.com>
2024-02-10 19:52:07 -0500

    Fixing Compute Engine

--- test/TestComputeEngineAPI.java
+++ test/TestComputeEngineAPI.java
@@ -1,17 +1,26 @@
 package test;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.any;
 import org.junit.Test;
 import src.ComputeEngineAPI;
 
-public class TestComputeEngineAPI 
+public class TestComputeEngineAPI
 {
     @Test
-    public void testComputeEngineAPI() 
+    public void testAlgorithmCompute() 
     {
         ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
         when(ce.compute(any(Integer.class))).thenReturn("1");
-        ce.compute(1);
+        ce.compute("1");
+    }
+    @Test
+    public void testRequest() 
+    {
+        ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
+        when(ce.compute(any(ComputeRequest.class)).thenReturn(ComputeResult.SUCCESS);
+        ComputeRequest request = new ComputeRequest();
+        ce.compute(request);
     }
 }
