51e4ae27858d433a0c2bc6cd180957705cb83cd7
Yoosup Song <Youssupsong@gmail.com>
2024-02-18 13:41:21 -0500

    Updated Compute Engine
    Finished functionality for compute method for Compute Engine Coordinator + edited files to match method names/parameters

--- test/TestComputeEngineAPI.java
+++ test/TestComputeEngineAPI.java
@@ -1,4 +1,5 @@
 package test;
+
 import org.mockito.Mockito;
 
 import project.OutputConfig;
@@ -18,18 +19,19 @@ import src.ClientAPI;
 import src.ComputeEngineAPI;
 import src.DataStorageAPI;
 
-public class TestComputeEngineAPI{
+public class TestComputeEngineAPI {
     @Test
-    public void testAlgorithmCompute(){
+    public void testAlgorithmCompute() {
         ComputeEngineAPI ce = Mockito.mock(ComputeEngineAPI.class);
         when(ce.compute(any(Integer.class))).thenReturn("1");
-        ce.compute(1);
+        ce.compute(1, 2);
     }
+
     @Test
-    public void testRequest(){
+    public void testRequest() {
         ComputeEngineAPI computeEngine = Mockito.mock(ComputeEngineAPI.class);
         when(computeEngine.compute(any(ComputingRequest.class)))
-        .thenReturn(ComputingResult.SUCCESS);
+                .thenReturn(ComputingResult.SUCCESS);
         ComputingRequest request = new ComputingRequest();
         computeEngine.compute(request);
     }
@@ -38,25 +40,27 @@ public class TestComputeEngineAPI{
 class ComputeEngineIntegrationTest {
     DataStorageAPI datastore = new DataStorageAPI();
     ComputeEngineAPI computeEngine = new ComputeEngineAPI();
-    public void testComputeEngineIntegration(){
+
+    public void testComputeEngineIntegration() {
         ClientAPI clientApi = new ClientAPI();
-        //It should have pointer, poitning to the Compute Engine
-        //And the Test Data Storage
+        // It should have pointer, poitning to the Compute Engine
+        // And the Test Data Storage
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(1);
         list.add(10);
         list.add(25);
-        //We add the list number to the Client API
-        //We run it fully thorugh the program, into the Data Strorage as well,
-        //we should a output Config of Array of string
-        clientApi.getInputConfig(list);                                                           
-        datastore.appendSingleResult(any(OutputConfig.class), 
-        computeEngine.compute(datastore.read(list)));
+        // We add the list number to the Client API
+        // We run it fully thorugh the program, into the Data Strorage as well,
+        // we should a output Config of Array of string
+        clientApi.getInput(list);
+        datastore.appendSingleResult(any(OutputConfig.class),
+                computeEngine.compute(datastore.read(list)));
         clientApi.getOutputConfig();
     }
+
     @Test
-    public void validation(OutputConfig output){                //user input
-        //if(output == computeEngine.compute(output)){}
+    public void validation(OutputConfig output) { // user input
+        // if(output == computeEngine.compute(output)){}
     }
 }
-//I know this is all wrong I'm lost
+// I know this is all wrong I'm lost
